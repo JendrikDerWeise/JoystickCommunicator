c6d9afa74b2f724d85013c7a7ff1396e693c51cc
JendrikDerWeise <jendrik.bulk@hs-bremen.de>
2025-05-07 12:23:59 +0200

    WheelchairControlReal sollte echten Stuhl steuern

--- .venv/Scripts/Server.py
+++ .venv/Scripts/Server.py
@@ -292,10 +292,10 @@ def run_server():
 
                         direction = (x, y)
                         wheelchair.set_direction(direction)
-                    #elif topic == b"gear":
-                        #received_value = from_network_order(message, '?')
-                        #actual_gear = wheelchair.set_gear(received_value)
-                        #publisher_socket.send_multipart([b"gear", to_network_order(actual_gear, 'i')])
+                    elif topic == b"gear":
+                        received_value = from_network_order(message, '?')
+                        actual_gear = wheelchair.set_gear(received_value)
+                        publisher_socket.send_multipart([b"gear", to_network_order(actual_gear, 'i')])
                     elif topic == b"lights":
                         received_value = from_network_order(message, '?')
                         wheelchair.set_lights()
